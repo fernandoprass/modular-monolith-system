cd537db4ecef4c7b4b76d4cffd7438cf21977f26
fernandoprass <fprass@gmail.com>
2026-06-09 17:33:30 +0100

    Front: Rename main folder

--- src/03.IAM/IAM.Domain/IamPermission.cs
+++ src/03.IAM/IAM.Domain/IamPermission.cs
@@ -21,6 +21,7 @@ public static class IamPermission
 
       public const string List = $"{Module}.{Resource}.list";
       public const string View = $"{Module}.{Resource}.view";
+      public const string ViewMe = $"{Module}.{Resource}.viewme";
       public const string Create = $"{Module}.{Resource}.create";
       public const string Update = $"{Module}.{Resource}.update";
       public const string UpdateMe = $"{Module}.{Resource}.updateme";
